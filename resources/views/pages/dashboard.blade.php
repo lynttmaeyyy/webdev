c97c52c7f81fa2d4d9c5afb7f42815b77a315504
lynette <lynettemacuto906@gmail.com>
2024-05-24 15:49:17 -0800

    msg

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -92,7 +92,7 @@
                         <div class="row">
                             <div class="col-12">
                                 <div class="numbers">
-                                    <p class="">Employee</p>
+                                    <p class="">Employees</p>
                                 </div>
                                 <table class="table">
                                     <thead class="thead">
